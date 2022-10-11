0f527d5f9ed6a1081f543d4fa9c6463ee1f7f40b
honghamin <113421639+honghamin@users.noreply.github.com>
2022-10-11 20:28:21 +0900

    06-05

--- 06-01.c
+++ 06-01.c
@@ -3,13 +3,33 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-void square(int a)
-{
-	a=a*a;
+int combination(int n, int r);
+int main(void){
+	
+	int n,r;
+	
+	printf("enter the n: ");
+	scanf("%d", &n);
+	
+	printf("enter the r: ");
+	scanf("%d", &r);
+	
+	printf("the result is %d", combination(n,r));
+	
 }
 
-int main(){
-	int a=2;
-	square(a);
-	printf("a=%i\n", a);
+int factorial(int n){
+	int i;
+	int res=1;
+	for(i=1;i<n;i++)
+	    res=res*i;
+	return res;
 }
+
+
+int combination(int n, int r)
+{
+	return (factorial(n)/(factorial(n-r)*factorial(r)));
+	
+}
+
