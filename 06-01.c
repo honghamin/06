6d7e59352ac3c0a198f55ce6d75412c197fab548
honghamin <113421639+honghamin@users.noreply.github.com>
2022-10-11 17:33:05 +0900

    06-04

--- 06-01.c
+++ 06-01.c
@@ -3,28 +3,13 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int sumTwo( int a, int b){
-	int sum = a+b;
-	return sum;
+void square(int a)
+{
+	a=a*a;
 }
-	
-int square (int n){
-	return (n*n);
-}
-	
-int get_max(int x, int y){
-	if (x < y)
-	    return x;
-	else
-	    return y;
-}	
 
-int main(int argc, char *argv[]) {
-	int res_sum, res_square, res_get_max;
-	res_sum = sumTwo(2,3);
-	res_square = square(5);
-	res_get_max = get_max(10,-20);
-	printf("%i, %i, %i", res_sum, res_square, res_get_max);
-	
-	return 0;
+int main(){
+	int a=2;
+	square(a);
+	printf("a=%i\n", a);
 }
